75735609bd351602e97ace1c708c09efd804098e
Agbaje Franklin <franchoal1919@gmail.com>
2026-07-19 15:13:50 +0100

    modify build

--- oja_oko_backend/build.sh
+++ oja_oko_backend/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 pip install -r requirements.txt
 
-python manage.py migrate
+python manage.py showmigrations products
+
+python manage.py migrate --verbosity 3
 
 python manage.py collectstatic --noinput
